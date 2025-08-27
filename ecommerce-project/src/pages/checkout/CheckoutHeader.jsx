1264230dab0b99eb295fafd2d6f44ede6562e56d
NdragovTest <ndragov99@abv.bg>
2025-08-27 17:26:25 +0300

    Display cart quantity

--- ecommerce-project/src/pages/checkout/CheckoutHeader.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutHeader.jsx
@@ -4,7 +4,13 @@ import Logo from '../../assets/images/logo.png';
 import MobileLogo from '../../assets/images/mobile-logo.png';
 import './CheckoutHeader.css';
 
-export function CheckoutHeader() {
+export function CheckoutHeader({ cart }) {
+    let totalQuantity = 0;
+
+    cart.forEach((cartItem) => {
+        totalQuantity += cartItem.quantity;
+    });
+
     return (
         <>
             <div className='checkout-header'>
@@ -12,17 +18,14 @@ export function CheckoutHeader() {
                     <div className='checkout-header-left-section'>
                         <Link to='/'>
                             <img className='logo' src={Logo} />
-                            <img
-                                className='mobile-logo'
-                                src={MobileLogo}
-                            />
+                            <img className='mobile-logo' src={MobileLogo} />
                         </Link>
                     </div>
 
                     <div className='checkout-header-middle-section'>
                         Checkout (
                         <Link className='return-to-home-link' to='/'>
-                            3 items
+                            {totalQuantity} items
                         </Link>
                         )
                     </div>
