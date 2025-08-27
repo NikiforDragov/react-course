9e0d25bc33f5db90205618698b444e4d0f41f2c5
NdragovTest <ndragov99@abv.bg>
2025-08-27 18:38:00 +0300

    Finish delivery status in tracking page

--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -35,6 +35,11 @@ export function TrackingPage({ cart }) {
     if (deliveryPercent > 100) {
         deliveryPercent = 100;
     }
+
+    const isPreparing = deliveryPercent < 33;
+    const isShipped = deliveryPercent >= 33 && deliveryPercent < 100;
+    const isDelivered = deliveryPercent === 100;
+
     return (
         <>
             <title>Tracking</title>
@@ -56,7 +61,9 @@ export function TrackingPage({ cart }) {
                     </Link>
 
                     <div className='delivery-date'>
-                        Arriving on:
+                        {deliveryPercent >= 100
+                            ? 'Delivered on '
+                            : 'Arriving on '}
                         {dayjs(orderProduct.estimatedDeliveryTimeMs).format(
                             'dddd, MMMM D'
                         )}
@@ -76,11 +83,27 @@ export function TrackingPage({ cart }) {
                     />
 
                     <div className='progress-labels-container'>
-                        <div className='progress-label'>Preparing</div>
-                        <div className='progress-label current-status'>
+                        <div
+                            className={`progress-label ${
+                                isPreparing && 'current-status'
+                            }`}
+                        >
+                            Preparing
+                        </div>
+                        <div
+                            className={`progress-label ${
+                                isShipped && 'current-status'
+                            }`}
+                        >
                             Shipped
                         </div>
-                        <div className='progress-label'>Delivered</div>
+                        <div
+                            className={`progress-label ${
+                                isDelivered && 'current-status'
+                            }`}
+                        >
+                            Delivered
+                        </div>
                     </div>
 
                     <div className='progress-bar-container'>
